a39070b791620c25e9cba3e7bf51dc1c5a6c4f5b
Edgar-Blancas-Morales <eddmorales10@gmail.com>
2020-06-08 22:24:21 -0500

    Terminando proyecto de ecommerce

---
+++ frontend/controladores/plantilla.controlador.php
@@ -0,0 +1,15 @@
+<?php
+
+class ControladorPlantilla{
+
+	public function plantilla(){
+		include "vistas/plantilla.php";
+	}
+
+	public function ctrEstiloPlantilla(){
+		$tabla = "plantilla";
+		$respuesta = ModeloPlantilla::mdlEstiloPlantilla($tabla);
+		return $respuesta;
+	}
+
+}
